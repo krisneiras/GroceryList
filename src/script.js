66d6ef0609a6490712efc98d96874fa893dd56d6
krisneiras <kristygns21@gmail.com>
2024-01-26 11:08:23 +0100

    viewLis edited

--- src/script.js
+++ src/script.js
@@ -1,10 +1,20 @@
 let listTag = document.getElementById("list");
 
 // READ
-async function viewList() {
+/* async function getList() {
     const result = await ("http://localhost:3000/products");
     const data = await result.json();
     return data;
+}
+ */
+async function viewList() {
+    const result = await ("http://localhost:3000/products");
+    const products = await result.json();
+    products.map(product => {
+        listTag.innerHTML +=
+        `<h3>${product.name}</h3>`
+        
+    })
 }
 
 //POST
