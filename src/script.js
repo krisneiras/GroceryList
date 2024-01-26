2831962ef24cbcc79cd42483ec614ca939301094
mariandrean <m.andreaan@gmail.com>
2024-01-26 19:42:56 +0100

    fix delete-button in viewList function - was missing quotation marks

--- src/script.js
+++ src/script.js
@@ -10,37 +10,36 @@ let listTag = document.getElementById("list");
 async function viewList() {
     const result = await fetch("http://localhost:3000/products");
     const products = await result.json();
-    console.log(result);
     products.map(product => {
         listTag.innerHTML +=
-        `<h3>${product.name}</h3> 
-        <button>Editar</button>
-        <button class="delete-button" onclick="deleteProduct(${product.id})">Eliminar</button>`
+            `<h3>${product.name}</h3> 
+            <button>Editar</button>
+            <button class="delete-button" onclick="deleteProduct('${product.id}')">Eliminar</button>`
     })
-
-    return products
+    return products;
 }
 //DELETE
-async function deleteProduct(id){
-    const result = await fetch(`http://localhost:3000/products/${id}`, {method: "DELETE"});
+async function deleteProduct(id) {
+    const result = await fetch(`http://localhost:3000/products/${id}`, { method: "DELETE" });
     return result;
 }
 
 //POST
 
 
-async function addProduct(){
-/*     const newProduct = document.getElementById("product-form");
-    console.log(newProduct.elements[0].value);
-     */
-    const result = await fetch(`http://localhost:3000/products/`, 
-    {method: "POST",
-    headers: {"Content-type": "application/json"},   
-    body: `{
-        "name": "Leche",
-        "quantity": 1 
-    }`
-});
+async function addProduct() {
+    /*const newProduct = document.getElementById("product-form");
+    console.log(newProduct.elements[0].value);*/
+
+    const result = await fetch(`http://localhost:3000/products/`,
+        {
+            method: "POST",
+            headers: { "Content-type": "application/json" },
+            body: `{
+                "name": "Leche",
+                "quantity": 1 
+            }`
+        });
     return result;
 }
 
