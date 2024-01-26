b073589ef04a61d8f515b5437b2d85e5ef55febc
krisneiras <kristygns21@gmail.com>
2024-01-26 10:52:41 +0100

    add function viewList

--- src/script.js
+++ src/script.js
@@ -2,7 +2,9 @@ let listTag = document.getElementById("list");
 
 // READ
 async function viewList() {
-
+    const result = await ("http://localhost:3000/products");
+    const data = await result.json();
+    return data;
 }
 
 //POST
