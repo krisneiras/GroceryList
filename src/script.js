f7960f624eae92841a910844a05bb9ad52c90817
krisneiras <kristygns21@gmail.com>
2024-01-26 12:07:59 +0100

    add deleteProduct

--- src/script.js
+++ src/script.js
@@ -13,14 +13,19 @@ async function viewList() {
     console.log(result);
     products.map(product => {
         listTag.innerHTML +=
-        `<h3>${product.name}</h3>`
+        `<h3>${product.name}</h3> 
+        <button>Editar</button>
+        <button class="delete-button" onclick="deleteProduct(${product.id})">Eliminar</button>`
     })
 
     return products
 }
+//DELETE
+async function deleteProduct(id){
+    const result = await fetch(`http://localhost:3000/products/${id}`, {method: "DELETE"});
+    return result;
+}
 
 //POST
-
-//DELETE
 
 //PUT
