5f82d758908d2a3207b3d80746868e7d05783cea
krisneiras <kristygns21@gmail.com>
2024-01-29 13:13:50 +0100

    function displayForm added

--- src/script.js
+++ src/script.js
@@ -14,10 +14,10 @@ async function viewList() {
     listTag.innerHTML = "";
     products.map(product => {
         listTag.innerHTML +=
-            `<li id="${product.id}">
+            `<li id="product-${product.id}">
                 <h3>${product.name}</h3>
                 <p>Cantidad:${product.quantity}</p> 
-                <button onclick="modifyProduct('${product.id}')">Editar</button>
+                <button id="edit-button" onclick="displayForm('${product.id}')">Editar</button>
                 <button class="delete-button" onclick="deleteProduct('${product.id}')">Eliminar</button>
             </li>`
     })
@@ -48,17 +48,42 @@ async function addProduct() {
 
 //PUT
 
-async function modifyProduct(id) {
-    const newProduct = document.getElementById("product-form");
+async function displayForm(id) {
+
+    document.getElementById(`product-${id}`).innerHTML+= `
+    <form action="" method="" id="modify-form">
+    <label for="modify-name">Producto: </label><input id="modify-name" name="name" type="text">
+    <label for="modify-quantity">Cantidad: </label><input id="modify-quantity" name="quantity" type="number" min="0">
+    <button type="submit" onclick="modifyProduct('${id}')">Guardar</button>
+</form>`
+
+}
+
+  async function modifyProduct(id) {
+    const modifyProduct = document.getElementById("modify-form");
 
     const result = await fetch(`http://localhost:3000/products/${id}`,
         {
             method: "PUT",
             headers: { "Content-type": "application/json" },
             body: `{
-                "name": "${newProduct.elements[0].value}",
-                "quantity": ${newProduct.elements[1].value} 
+                "name": "${modifyProduct.elements[0].value}",
+                "quantity": ${modifyProduct.elements[1].value} 
             }`
         });
     return result;
 }
+
+/* const editButton = document.getElementById('edit-button');
+
+editButton.addEventListener('click', () => {
+    const editForm = document.getElementById(modify-form);
+
+    if(form.style.display === 'none') {
+        form.style.display = 'block';
+    }
+
+    else{
+        form.style.display = 'none'
+    }
+}) */
